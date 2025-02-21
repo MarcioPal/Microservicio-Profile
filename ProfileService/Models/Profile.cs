1577697601f0f9c8f206e074acdbf42c9cec2bcf
Marcio <marciopalazzo22@gmail.com>
2025-02-21 09:58:50 -0300

    Modifico propiedades de Propiedades de Profile

--- ProfileService/Models/Profile.cs
+++ ProfileService/Models/Profile.cs
@@ -1,16 +1,24 @@
-﻿namespace ProfileService.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ProfileService.Models
 {
     public class Profile
     {
-        public string id { get; set; }
+        [BsonId] 
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BindNever]
+        public string? id { get; set; }
+        [BindNever]
         public string userId { get; set; }
         public string name { get; set; }
         public string lastname { get; set; } 
         public string birthdate { get; set; }
         public string phone { get; set; }
-        public string imageId { get; set; }
-        public string tag_id { get; set; }
-        public Address address { get; set;}
+        public string? imageId { get; set; }
+        public string? tag_id { get; set; }
+        public Address? address { get; set;}
 
 
     }
